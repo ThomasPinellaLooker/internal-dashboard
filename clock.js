239b01ed8c2c48d32c813db5b26f47dd0ce64f23
thomas.pinella@looker.com <thomas.pinella@looker.com>
2018-01-31 13:35:38 -0500

    Digital Clock operational

--- clock.js
+++ clock.js
@@ -22,59 +22,79 @@
     // Set up the initial state of the visualization
     create: function (element, config) {
 
+      this._clock = element.appendChild(document.createElement("div"));
 
+      let myThis = this;
+      this._interval = setInterval(function() {
+        calculateTime(myThis);
+      }, 1000);
     },
     // Render in response to the data or settings changing
     update: function (data, element, config, queryResponse) {
       this.clearErrors();
+      let myThis = this;
+      clearInterval(this._interval);
 
-      let localTime = new Date().getTime();
-      let localOffset = new Date().getTimezoneOffset()*60000;
-      let utc = localTime + localOffset;
-      let mult = 3600000;
+      calculateTime(myThis);
+      this._interval = setInterval(function() {
+        calculateTime(myThis);
+      }, 1000);
 
-      let caliOffset = -7;
-      let nycOffset = -4;
-      let chiOffset = -5;
-      let ukOffset = 1;
+      function calculateTime(myThis) {
+        let localTime = new Date().getTime();
+        let localOffset = new Date().getTimezoneOffset() * 60000;
+        let utc = localTime + localOffset;
+        let mult = 3600000;
 
-      switch (Number(config.city)) {
-        case 0:
-        case 1:
-          let caliTime = new Date(utc + (mult * caliOffset));
-          if (!isDST(caliTime, 2, 2, 10, 1)) {
-            caliOffset--;
-            caliTime = new Date(utc + (mult * caliOffset));
-          }
-          console.log(caliTime);
-          break;
-        case 2:
-          let nycTime = new Date(utc + (mult * nycOffset));
-          if (!isDST(nycTime, 2, 2, 10, 1)) {
-            nycOffset--;
-            nycTime = new Date(utc + (mult * nycOffset));
-          }
-          console.log(nycTime);
-          break;
-        case 3:
-          let chiTime = new Date(utc + (mult * chiOffset));
-          if (!isDST(chiTime, 2, 2, 10, 1)) {
-            chiOffset--;
-            chiTime = new Date(utc + (mult * chiOffset));
-          }
-          console.log(chiTime);
-          break;
-        case 4:
-        case 5:
-          let ukTime = new Date(utc + (mult * ukOffset));
-          if (!isDST(ukTime, 2, 2, 10, 1)) {
-            ukOffset--;
-            ukTime = new Date(utc + (mult * ukOffset));
-          }
-          console.log(ukTime);
+        let caliOffset = -7;
+        let nycOffset = -4;
+        let chiOffset = -5;
+        let ukOffset = 1;
+
+        let time = new Date();
+
+        switch (Number(config.city)) {
+          case 0:
+          case 1:
+            let caliTime = new Date(utc + (mult * caliOffset));
+            if (!isDST(caliTime, 2, 2, 10, 1)) {
+              caliOffset--;
+              caliTime = new Date(utc + (mult * caliOffset));
+            }
+            time = caliTime;
+            break;
+          case 2:
+            let nycTime = new Date(utc + (mult * nycOffset));
+            if (!isDST(nycTime, 2, 2, 10, 1)) {
+              nycOffset--;
+              nycTime = new Date(utc + (mult * nycOffset));
+            }
+            time = nycTime;
+            break;
+          case 3:
+            let chiTime = new Date(utc + (mult * chiOffset));
+            if (!isDST(chiTime, 2, 2, 10, 1)) {
+              chiOffset--;
+              chiTime = new Date(utc + (mult * chiOffset));
+            }
+            time = chiTime;
+            break;
+          case 4:
+          case 5:
+            let ukTime = new Date(utc + (mult * ukOffset));
+            if (!isDST(ukTime, 2, 2, 10, 1)) {
+              ukOffset--;
+              ukTime = new Date(utc + (mult * ukOffset));
+            }
+            time = ukTime;
+        }
+
+        myThis._clock.innerHTML = "";
+
+        let display = myThis._clock.appendChild(document.createElement("h1"));
+
+        display.innerHTML = time.toLocaleTimeString();
       }
-
-
 
       function isDST(date, beginsMonth, beginsSunday, endsMonth, endsSunday) {
 
